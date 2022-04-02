66d5c2fde664f67c8bb3251e25f7b052f9499feb
Misha Pozdniakov <misha.pozd@gmail.com>
2022-04-02 14:44:34 -0400

    Added Events to game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public Hexsphere ActivePlanet;
+    public UnityEvent OnGameOver;
 
     [HideInInspector] public Tile SelectedTile;
 
