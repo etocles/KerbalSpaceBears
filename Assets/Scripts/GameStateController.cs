7a24aacc57b667f6a512b7c22fd78aefd651352b
andrewsirkoch <sirkoa@rpi.edu>
2022-04-02 13:03:38 -0400

    plantary gen scene x2

--- Assets/Scripts/GameStateController.cs
+++ Assets/Scripts/GameStateController.cs
@@ -59,7 +59,7 @@ public class GameStateController : MonoBehaviour
             spawnedPlanet.transform.localPosition = new Vector3(definition.orbitalRadius, 0, 0);
             planetPivot.transform.localEulerAngles = new Vector3(0, Random.Range(0, 360), 0);
             Rotate rotCtrl = planetPivot.AddComponent<Rotate>();
-            rotCtrl.speed += rotCtrl.speed * (definition.orbitalRadius / 200);
+            rotCtrl.speed -= rotCtrl.speed * (definition.orbitalRadius / 200);
             if (Random.value > 0.5f) rotCtrl.speed *= -1f;
         }
     }
